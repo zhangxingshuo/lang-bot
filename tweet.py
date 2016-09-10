a2093f7dc8429586c48a164267267574d21d7edb
Andy Zhang <axzhang@hmc.edu>
2016-09-10 16:49:48 -0700

    Added markov chain

---
+++ tweet.py
@@ -0,0 +1,15 @@
+import tweepy
+
+from keys import *
+from markov import Markov
+
+auth = tweepy.OAuthHandler(consumerKey, consumerKeySecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+api = tweepy.API(auth)
+
+markov = Markov('tale_two_cities.txt')
+
+tweet = markov.generate_text[:140]
+
+api.update_status(tweet)
