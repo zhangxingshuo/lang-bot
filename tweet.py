f6df7229061deba46db94e837bd0c5525a7b3e15
Andy Zhang <axzhang@hmc.edu>
2016-09-10 20:58:39 -0700

    Added file argument

--- tweet.py
+++ tweet.py
@@ -1,14 +1,19 @@
 import tweepy
+import argparse
 
 from keys import *
 from markov import Markov
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-f', '--file', help="path to file")
+args = ap.parse_args()
 
 auth = tweepy.OAuthHandler(consumerKey, consumerKeySecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 
 api = tweepy.API(auth)
 
-markov = Markov('tale_two_cities.txt')
+markov = Markov(args.file)
 
 tweet = markov.generate_text()[:140]
 
