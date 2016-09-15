0ee63ba57935852f4bec7996e0a151014328d491
Andy Zhang <axzhang@hmc.edu>
2016-09-14 23:46:40 -0700

    new post

--- text_gen.py
+++ text_gen.py
@@ -13,16 +13,22 @@ import sys
 
 from markov import Markov
 
-def generate_text(filename):
+def generate_text(filenames):
     '''
     Using python markovify package
     '''
-    with open(filename) as f:
-        text = f.read()
+    text_models = []
 
-    text_model = markovify.Text(text)
+    for filename in filenames:
 
-    return text_model.make_short_sentence(140)
+        with open(filename) as f:
+            text = f.read()
+
+        text_models.append(markovify.Text(text))
+
+    total_model = markovify.combine(text_models)
+
+    return total_model.make_short_sentence(140)
 
 def markov_generate_text(filename):
     '''
@@ -41,5 +47,5 @@ def markov_generate_text(filename):
     return text
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    print(generate_text(filename))
+    filenames = sys.argv[1:]
+    print(generate_text(filenames))
